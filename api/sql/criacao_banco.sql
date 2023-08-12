7543cb8c152e88508348f7118eb4ded9c0e6d7f2
ricardospinoza <ricardospinoza@gmail.com>
2023-08-12 16:45:43 -0300

    script de banco

---
+++ api/sql/criacao_banco.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE if not EXISTS devbook;
+
+USE devbook;
+
+DROP TABLE IF EXISTS usuarios;
+
+create TABLE usuarios(
+    id int auto_increment primary key,
+    nome varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    senha varchar(20) not null unique,
+    criadoEm timestamp default current_timestamp()
+
+) ENGINE=INNODB;

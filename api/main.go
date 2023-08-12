2f3e01365f348d20e2782101624c749123baac1b
ricardospinoza <ricardospinoza@gmail.com>
2023-08-12 15:10:01 -0300

    add pacotes main e router

---
+++ api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	router "api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Rodando API!")
+	r := router.Gerar()
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
